cc4d1e4b413e1496bed86a4de39e416df223f26f
gargjain <jaingargi774@gmail.com>
2025-09-21 15:42:31 +0530

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int asciiCode;
+
+    // Input ASCII code
+    cout << "Enter an 9ASCII code (0-127): ";
+    cin >> asciiCode;
+
+    // Print corresponding character
+    cout << "The character for ASCII code " << asciiCode << " is: " << char(asciiCode) << endl;
+
+    return 0;
+}
