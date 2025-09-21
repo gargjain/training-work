38260a5a1648d32d25cb5177889e027b4a9b6920
gargjain <jaingargi774@gmail.com>
2025-09-21 15:42:11 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    // Input character
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    // Print ASCII code
+    cout << "The ASCII code of '" << ch << "' is: " << int(ch) << endl;
+
+    return 0;
+}
